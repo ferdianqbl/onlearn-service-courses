7320e6ac7dd79d73d7ad9e2e5bcb9e764e2f2a0a
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-04-30 21:14:34 +0700

    create add mentor API

---
+++ app/Http/Controllers/MentorController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mentor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class MentorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        $rules = [
+            'name' => 'required|string',
+            'profile' => 'required|url',
+            'profession' => 'required|string',
+            'email' => 'required|email',
+        ];
+
+        $data = $request->all();
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails())
+            return response()->json([
+                'status' => '1',
+                'message' => $validator->errors()
+            ], 400);
+
+        $mentor = Mentor::create($data);
+
+        return response()->json([
+            'status' => '1',
+            'message' => 'Mentor successfully created',
+            'data' => $mentor
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mentor $mentor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Mentor $mentor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Mentor $mentor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mentor $mentor)
+    {
+        //
+    }
+}
--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -10,6 +10,10 @@ class Mentor extends Model
     use HasFactory;
 
     protected $table = 'mentors';
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+    ];
     protected $fillable = [
         'name', 'email', 'profile', 'profession'
     ];
