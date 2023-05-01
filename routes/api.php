0cc951dcd24605597a5d8470bb180e8146c3d1fe
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-05-01 09:58:17 +0700

    grouping router

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/mentors', [MentorController::class, 'create']);
+Route::controller(MentorController::class)->group(function () {
+    Route::post('/mentors', 'create');
+});
