e8c9dbded6ee25c5236dab17df23b1e63d7ef204
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-08-06 00:05:06 +0700

    feat: create helpers to get user Data

---
+++ app/helpers.php
@@ -0,0 +1,47 @@
+<?php 
+use Illuminate\Support\Facades\Http;
+
+function getUser($userId) {
+  $url = env('SERVICE_USER_URL') . '/users/' . $userId;
+
+  try {
+    $response = Http::timeout(10)->get($url);
+    $data = $response->json();
+    $data['http_code'] = $response->getStatusCode();
+    return $data;
+    
+  } catch (\Throwable $th) {
+    return [
+      'error' => 1,
+      'http_code' => 500,
+      'message' => 'Service user unavailable'
+    ];
+  }
+}
+
+function getUserByIds($userIds=[]) {
+  $url = env('SERVICE_USER_URL') . '/users/';
+
+  try {
+    if(count($userIds) == 0) {
+      return [
+        'error' => 0,
+        'http_code' => 200,
+        'data' => []
+      ];
+    }
+    $response = Http::timeout(10)->get($url, ['user_ids[]' => $userIds]);
+    $data = $response->json();
+    $data['http_code'] = $response->getStatusCode();
+    return $data;
+    
+  } catch (\Throwable $th) {
+    return [
+      'error' => 1,
+      'http_code' => 500,
+      'message' => 'Service user unavailable'
+    ];
+  }
+}
+
+?>
